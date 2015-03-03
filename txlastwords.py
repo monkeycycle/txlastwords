da7a6aff01bbb681af7e94b75d687d67d7e355d0
59dfda23 <59dfda23@opayq.com>
2015-03-03 11:52:11 -0600

    committed again to add correct initial file

--- txlastwords.py
+++ txlastwords.py
@@ -1,24 +1,35 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
-html = urllib2.urlopen('http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html')
+# part 1
+
+html = urllib2.urlopen('http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html').read()
+
+# part 2
 
 soup = BeautifulSoup(html)
 results_table = soup.find('table', attrs={'class': 'os'})
 
+
+# part 3
+
 for tr in results_table.findAll('tr'):
-	print ROW 
+    # print 'ROW'
 
     for td in tr.findAll('td'):
-    	print td.text
-        
-#         soup.find('td', attrs={'class':'a href'})
-#         soup.find('td', attrs={'class':'Last Name'})
-#         soup.find('td', attrs={'class':'First Name'})
-#         soup.find('td', attrs={'class':'First Name'})
+        # print td.text
+
+        if td.text == "Last Statement": 
+            # If the text has "Last Statement" in it, then... 
+            last_words = urllib2.urlopen([soup.find('a', attrs='href')]).read()
+            # open the link. 
+            body = soup.find('div', attrs='id': 'body')
+            # Then go to the text div... 
+            for p in body: 
+                # find the last paragraph tag... 
+                print p.text 
+                # and print the text. 
 
 
-#     output_list.append(row_list)
-
-# print LAST WORDS 
-# print output_list
+# for p[-1] in body: 
+#     print p 
