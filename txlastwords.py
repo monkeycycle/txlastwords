4ed4e0a9626ae270472db3d8e49a314295106587
Chase Davis <chase.davis@gmail.com>
2015-03-08 16:44:18 -0400

    Couple hints

--- txlastwords.py
+++ txlastwords.py
@@ -20,7 +20,16 @@ for tr in results_table.findAll('tr'):
 
         if td.text == "Last Statement": 
             # If the text has "Last Statement" in it, then... 
-            last_words = urllib2.urlopen([soup.find('a', attrs='href')]).read()
+            
+            # Try this to get the link. It grabs the "a" tag from the td you're currently in (rather than the "soup" variable,
+            # which represents the whole document. The ['href'] bit at the end is how you get the href attribute.
+            relative_link = td.find('a')['href']
+            
+            # Next you'll need to turn this into an absolute link by appending http://www.tdcj.state.tx.us/death_row/ to the front.
+            
+            # Then you'll open it using urllib2.urlopen, read the HTML and scrape the bit you're looking for.
+
+            #last_words = urllib2.urlopen([soup.find('a', attrs='href')]).read()
             # open the link. Problem: every link is different because they are named after the person. The most recent one, for example, is <a href="dr_info/newburydonaldlast.html">Last Statement</a> How do we get it to go to every link? We know we have to do it in the key-value pair, but how do we put changing text in the link? 
             body = soup.find('div', attrs='id': 'body')
             # Then go to the text div on the person's page. 
